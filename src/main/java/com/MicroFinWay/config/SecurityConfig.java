e627ab42efcae31286074138eacb5d7e08c125da
whoami-789 <tudya1238@gmail.com>
2025-10-07 01:25:21 +0500

    db + auth jwt config

--- src/main/java/com/MicroFinWay/config/SecurityConfig.java
+++ src/main/java/com/MicroFinWay/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // 👈 preflight
                         .requestMatchers("/api/auth/login").permitAll()
-                        .requestMatchers("/api/auth/register").hasRole("ADMIN")
+                        .requestMatchers("/api/auth/register").permitAll()
                         .requestMatchers(
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
